cbcb4b3ee479b21d77e851dc601b3996a26e5812
wangcheng <3169833943@qq.com>
2024-02-29 14:17:38 +0800

    创建任务相关
    Signed-off-by: wangcheng <3169833943@qq.com>

--- src/main/java/com/laughbro/welcome/dao/mapper/UserMapper.java
+++ src/main/java/com/laughbro/welcome/dao/mapper/UserMapper.java
@@ -10,15 +10,13 @@ public interface UserMapper {
     /**
      * 【调用接口】 login
      * 【作用】 判断账号密码是否存在以及匹配
+     *
      * @param
      * @return 存在返回 该用户所有信息
-     *         不存在返回  null
+     * 不存在返回  null
      */
     @Select("select * from users where id=#{id}")
     public User login_select_user_exist_by_id(String id);
-
-
-
 
 
 }
