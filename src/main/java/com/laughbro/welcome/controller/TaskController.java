3aa303046e8ab25a43a61afa869f1e6340fa075c
王程 <9836466+w3169833943@user.noreply.gitee.com>
2024-03-01 14:52:24 +0800

    V.0.1
    Signed-off-by: wangcheng<3169833943@qq.com>

--- src/main/java/com/laughbro/welcome/controller/TaskController.java
+++ src/main/java/com/laughbro/welcome/controller/TaskController.java
@@ -1,4 +1,18 @@
 package com.laughbro.welcome.controller;
 
+import com.laughbro.welcome.service.TaskService;
+import com.laughbro.welcome.vo.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+    @PostMapping("/quest")
+    public Result GetTask(){
+        return Result.success(taskService.GetTask());
+    }
+
 }
