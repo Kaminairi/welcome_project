65d21e454c06e97e9114b6db5d90a7a74f2d3b9c
王程 <9836466+w3169833943@user.noreply.gitee.com>
2024-03-02 15:47:34 +0800

    V.0.1.1添加注释
    Signed-off-by: wangcheng<3169833943@qq.com>

--- src/main/java/com/laughbro/welcome/dao/mapper/TaskMapper.java
+++ src/main/java/com/laughbro/welcome/dao/mapper/TaskMapper.java
@@ -9,12 +9,33 @@ import java.util.List;
 
 @Mapper
 public interface TaskMapper {
+    /**
+     * 【调用接口】 quest/main
+     * 【作用】 查询主线任务信息
+     * @param
+     * @return 存在返回 所有主线任务
+     *         不存在返回  null
+     */
     @Select("select * from tasks where is_mainline=1")
     List<Task> select_task_main();
 
+    /**
+     * 【调用接口】 quest/nmain
+     * 【作用】 查询支线任务信息
+     * @param
+     * @return 存在返回 所有支线任务
+     *         不存在返回  null
+     */
     @Select("select * from tasks where is_mainline!=1")
     List<Task> select_task_nmain();
 
+    /**
+     * 【调用接口】 quest/finish
+     * 【作用】 通过id查询用户已完成任务
+     * @param
+     * @return 存在返回 完成的所有任务
+     *         不存在返回  null
+     */
     @Select("select * from tasks where id in (select task_id from task_fulfillment where user_id=#{id})")
     List<Task> select_task_finish(String id);
 }
--- src/main/java/com/laughbro/welcome/service/LoginService.java
+++ src/main/java/com/laughbro/welcome/service/LoginService.java
@@ -1,11 +1,8 @@
 package com.laughbro.welcome.service;
 
 
-import com.laughbro.welcome.dao.pojo.User;
 import com.laughbro.welcome.vo.Result;
 import com.laughbro.welcome.vo.params.login_params.LoginParams;
-
-import java.io.Reader;
 
 public interface LoginService {
 
