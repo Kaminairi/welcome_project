ea09f0d26a13f583bf5146bad25c90e7f42d1aee
kaminairi <1441847870@qq.com>
2024-03-12 16:51:33 +0800

    增加测试获得服务器url的接口 Signed-off-by: kaminairi <1441847870@qq.com>
    Signed-off-by: kaminairi <1441847870@qq.com>

--- src/main/java/com/laughbro/welcome/controller/TestController.java
+++ src/main/java/com/laughbro/welcome/controller/TestController.java
@@ -3,9 +3,12 @@ package com.laughbro.welcome.controller;
 import com.laughbro.welcome.service.SSEService;
 import com.laughbro.welcome.vo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
@@ -18,5 +21,20 @@ public class TestController {
     public Result testsse(String data) throws IOException {
         sseService.sseSendMessage("1111111111",data);
         return Result.success(null);
+    }
+
+    @GetMapping("/geturl")
+    public void geturl(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // 获取服务器的URL
+        StringBuffer serverURL = request.getRequestURL();
+        String contextPath = request.getContextPath();
+
+        // 构建完整的服务器URL
+        String fullServerURL = serverURL.toString().replace(request.getRequestURI(), contextPath);
+
+        // 将服务器URL字符串作为响应返回给客户端
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(fullServerURL);
     }
 }
